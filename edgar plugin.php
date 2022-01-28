1f296051993fc343bd65ab7b9f82cd8751fd682f
Ampeire <ampeireedgar5@gmail.com>
2022-01-28 10:18:43 +0300

    Initial Commit

---
+++ edgar plugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package EdgarPlugin
+ * @author Ampeire Edgar
+ * @license GPL v2 or later
+ * @copyright 2022 Ampeire Edgar All rights reserved
+ */
+/*
+ * Plugin Name: Edgar Plugin
+ * Plugin URI:
+ * Description: This plugin allows you to integrate the EgoSMS into your Wordpress site.
+ * Version: 1.0.0
+ * Author: Ampeire Edgar
+ * Author URI: https://github.com/dancan-edgar
+ * Text Domain: edgar plugin
+ * License: GPL v2 or later
+ */
